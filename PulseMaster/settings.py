d2495e04d9068352df0b6e1af023c066233bf981
ashiq <ishtiaqashiq5@gmail.com>
2022-04-28 13:34:45 -0400

    minor

--- PulseMaster/settings.py
+++ PulseMaster/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from .local import *
+# from .local import *
+LOCAL = False
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
